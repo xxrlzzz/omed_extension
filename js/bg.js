9ca1902b0458c1efdeea65c576960a588bad88d1
Haoshen Han <hanhaoshen@bytedance.com>
2020-12-16 11:08:55 +0800

    支持源码中的link(shift+right-click激活context)

--- js/bg.js
+++ js/bg.js
@@ -11,6 +11,10 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
 			params = url.split(':')[2].split(';')
 			filepath = params[0]
 			line = params[params.length - 1].split('=')[1]
+      line = parseInt(line.split('?')[0])
+      if (!line) {
+        line = 0
+      }
 			$.get('http://127.0.0.1:8989/file?f=' + filepath + '&l=' + line)
 			break;
 	}
